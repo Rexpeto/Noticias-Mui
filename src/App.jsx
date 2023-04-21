c5f43c0ee09b782b845947eeefa211cc6224a81a
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-21 16:56:09 -0400

    Use Component Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { Container, Grid, Typography } from "@mui/material";
+import Search from "./components/Search";
 
 const App = () => {
     return (
         <Container>
-            <Grid>
-                <header>
-                    <Typography variant="h3" component="h1" align="center" >
-                        Buscador de Noticias
-                    </Typography>
-                </header>
+            <header>
+                <Typography
+                    variant="h3"
+                    component="h1"
+                    align="center"
+                    sx={{
+                        marginBottom: 2,
+                    }}
+                >
+                    Buscador de Noticias
+                </Typography>
+            </header>
+            <Grid container direction="row" justifyContent="center">
+                <Grid item md={6} xs={12}>
+                    <Search />
+                </Grid>
             </Grid>
         </Container>
     );
