697cb751ffb015ed0dfe66a82e0b150056501f90
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-21 17:37:04 -0400

    Function Change Category

--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -3,10 +3,18 @@ import { createContext, useState, useEffect } from "react";
 const NewsContext = createContext();
 
 export const NewsProvider = ({ children }) => {
+    //* State Category
+    const [category, setCategory] = useState("general");
+
+    const handdlerChangeCategory = (e) => {
+        setCategory(e.target.value);
+    };
+
     return (
         <NewsContext.Provider
             value={{
-                hola: "hola",
+                handdlerChangeCategory,
+                category,
             }}
         >
             {children}
