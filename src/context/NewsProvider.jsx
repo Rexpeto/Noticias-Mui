cec55d8d26b7b6f59c198dbfdbfff554f9869afd
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-21 17:54:10 -0400

    Request Api news

--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -1,14 +1,35 @@
+import axios from "axios";
 import { createContext, useState, useEffect } from "react";
 
 const NewsContext = createContext();
 
 export const NewsProvider = ({ children }) => {
+    //* State article
+    const [news, setNews] = useState([]);
+
     //* State Category
     const [category, setCategory] = useState("general");
 
     const handdlerChangeCategory = (e) => {
         setCategory(e.target.value);
     };
+
+    //* UseEffect request api
+    useEffect(() => {
+        const requestNews = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=ve&category=${category}&apiKey=${
+                import.meta.env.VITE_API_KEY
+            }`;
+
+            const {
+                data: { articles },
+            } = await axios(url);
+
+            setNews(articles);
+        };
+
+        requestNews();
+    }, [category]);
 
     return (
         <NewsContext.Provider
